267d1013156e4d1a1d85ba9a7844292c1be61b9e
unknown <ash.chaudhry@yahoo.co.uk>
2022-04-02 19:24:55 +0100

    newgitclass

---
+++ GithubClass.java
@@ -0,0 +1,8 @@
+package com.learning.todaysclass;
+
+public class GithubClass {
+    public static void main(String[] args)
+    {
+        System.out.println("today's class was about github");
+    }
+}
