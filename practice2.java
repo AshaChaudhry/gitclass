305c557969049570935db8add9e50d94a6151726
unknown <ash.chaudhry@yahoo.co.uk>
2022-04-13 18:26:34 +0100

    for practice

---
+++ practice2.java
@@ -0,0 +1,27 @@
+package com.learning.todaysclass;
+
+public class practice2 {
+    public static void main(String[] args) {
+        int length=5;
+        int width=7;
+        int area;
+        int perimeter;
+        System.out.println("area:"+(length*width));
+        System.out.println("perimeter:"+(length+width));
+
+        int a=2345;
+       int b=a+8;
+
+        System.out.println(b/3);
+        System.out.println(((b/3)%5)*5);
+
+        System.out.println(23==45);
+
+        int A=55;
+        int B=70;
+        System.out.println(a<50&&a<b);
+        System.out.println(a<50||a<b);
+
+
+    }
+}
